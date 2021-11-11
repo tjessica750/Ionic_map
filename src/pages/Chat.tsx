c0b297adf4085f31e225319464bb539e0c09b13a
Jessica Torres <tjessica750@gmail.com>
2021-11-10 21:01:20 -0500

    v2

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -65,8 +65,8 @@ const Chat: React.FC = () => {
                             <img src="http://pm1.narvii.com/6513/891d31199d907db135932627c04bd737977ffc69_00.jpg" />
                         </IonAvatar>
                         <IonLabel>
-                            <h2>Jessica</h2>
-                            <h3>Hola Como estas ?</h3>
+                            <h2>Laura Montalvo</h2>
+                            <h3>Hola... </h3>
                         </IonLabel>
                     </IonItem>
                     <IonItem button={true}>
@@ -74,8 +74,8 @@ const Chat: React.FC = () => {
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZAWKHciGaR9qdyGy5li0CSLGiHjlYDPEKwA&usqp=CAU" />
                         </IonAvatar>
                         <IonLabel>
-                            <h2>Joel Dovale</h2>
-                            <h3>Hola Mi amor como estas ?</h3>
+                            <h2>Luis Perejildo</h2>
+                            <h3>Hola como estas ?</h3>
                         </IonLabel>
                     </IonItem>
                 </IonList>
